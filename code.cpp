6e68d430e52c7d1be0a92ba3b4e18b0dcc9426e8
fidofido21 <fido.osiashvili@gmail.com>
2016-11-07 15:52:56 +0400

    Update and rename .gitignore to code.cpp

---
+++ code.cpp
@@ -0,0 +1,11 @@
+public static string Hash(string text)
+{
+byte[] data = System.Security.Cryptography.MD5.Create().ComputeHash(System.Text.Encoding.UTF8.GetBytes(text));
+string md5 = "";
+for (int i = 0; i < data.Length; i++)
+{
+md5 += data[i].ToString("x2");
+}
+return md5;
+
+    }
